4c069240d11d27157e99fbfc7f2eb642b6fa1ab4
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-12-24 16:38:23 +0530

    App.jsx merged with useFetch Hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import useFetch from './hooks/useFetch'
 
 
 export default function App() {
+  
   let {loading, data, error} =useFetch('http://localhost:1337/api/blogs?populate=*')
   if(loading) return <p>Loading...</p>
   if(error) return <p>Error!</p>
